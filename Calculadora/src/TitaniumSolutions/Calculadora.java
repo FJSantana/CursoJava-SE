fbdedcca8060e25cf416e13c33f5bb429b882f4d
FJ Santana <santana0094@gmail.com>
2020-06-10 16:04:49 -0500

    Archivos Creados

---
+++ Calculadora/src/TitaniumSolutions/Calculadora.java
@@ -0,0 +1,32 @@
+package TitaniumSolutions;
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main (String[] args) {
+
+        Calcular calculo = new Calcular();
+        Scanner dato = new Scanner(System.in);
+
+        System.out.println("Ingresar primer dato");
+        int n1 = dato.nextInt();
+        System.out.println("Ingresar segundo dato");
+        int n2 = dato.nextInt();
+
+        calculo.setNum1(n1);
+        calculo.setNum1(n2);
+
+        System.out.println("SUMA");
+        System.out.println(calculo.suma(n1, n2));
+        System.out.println("RESTA");
+        System.out.println(calculo.resta(n1, n2));
+        System.out.println("MULTIPLICACION");
+        System.out.println(calculo.multiplicacion(n1, n2));
+        System.out.println("DIVISION" );
+        System.out.println(calculo.division(n1, n2));
+
+    }
+
+
+
+}
---
+++ Calculadora/src/TitaniumSolutions/Calcular.java
@@ -0,0 +1,48 @@
+package TitaniumSolutions;
+
+public class Calcular {
+
+    private int num1;
+    private int num2;
+
+    public void setNum1(int num1) {
+
+        this.num1 = num1;
+    }
+
+    public void setNum2(int num2) {
+
+        this.num2 = num2;
+    }
+
+    public int getNum1() {
+
+        return this.num1;
+    }
+
+    public int getNum2() {
+
+        return this.num2;
+    }
+
+    public int suma(int num1, int num2) {
+        int num3 = num1 + num2;
+        return num3;
+    }
+
+    public int resta(int num1, int num2) {
+        int num3 = num1 - num2;
+        return num3;
+    }
+
+    public int multiplicacion(int num1, int num2) {
+        int num3 = num1 * num2;
+        return num3;
+    }
+
+    public float division(int num1, int num2) {
+        float num3 = (num1 / num2);
+        return num3;
+    }
+
+}
